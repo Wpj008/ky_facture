9fe43e36109cb8a371c9eab934087e3d9ab39a5a
WPJ008 <wawitephilemon@gmail.com>
2026-07-04 17:27:49 +0200

    function customer

---
+++ functions/customers.php
@@ -0,0 +1,63 @@
+<?php
+require_once "../functions/database.php";
+
+
+function InsertCustomer($type, $lastname, $firstname, $entreprise, $adresse, $ville, $code_postal, $phone, $email){
+
+    try{
+
+    $queryInsert = getPDO()->prepare("INSERT INTO customers(type_customer, lastname_customer, firstname_customer, entreprise, adresse_customer, ville_customer, code_postal, phone_customer, email_customer, created_at_customer, updated_at_customer) VALUES(:type, :lastname, :firstname, :entreprise, :adresse, :ville, :code_postal, :phone, :email, NOW(), NOW())");
+    $queryInsert->bindParam(":type", $type);
+    $queryInsert->bindParam(":lastname", $lastname);
+    $queryInsert->bindParam(":firstname", $firstname);
+    $queryInsert->bindParam(":entreprise", $entreprise);
+    $queryInsert->bindParam(":adresse", $adresse);
+    $queryInsert->bindParam(":ville", $ville);
+    $queryInsert->bindParam(":code_postal", $code_postal);
+    $queryInsert->bindParam(":phone", $phone);
+    $queryInsert->bindParam(":email", $email);
+
+    $result = $queryInsert->execute();
+
+    header("Location: /facturation/pages/dashboard.php");
+    exit();
+
+
+    }catch(PDOException $e){
+        echo "Erreur lors de l'insertion du client : " . $e->getMessage();
+    }
+
+
+
+
+
+}
+
+
+function getAllCustomers(){
+
+    try{
+        $querySelect = getPDO()->prepare("SELECT * FROM customers");
+        $querySelect->execute();
+        $result = $querySelect->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }catch(PDOException $e){
+        echo "Erreur lors de la récupération des clients : " . $e->getMessage();
+    }
+}
+
+function getCustomerById($customerId){
+
+    try{
+        $querySelect = getPDO()->prepare("SELECT * FROM customers WHERE id_customer = :customerId");
+        $querySelect->bindParam(":customerId", $customerId);
+        $querySelect->execute();
+        $result = $querySelect->fetch(PDO::FETCH_ASSOC);
+        return $result;
+    }catch(PDOException $e){
+        echo "Erreur lors de la récupération du client : " . $e->getMessage();
+    }
+}   
+
+
+?>
