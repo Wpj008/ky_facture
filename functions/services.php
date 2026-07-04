c3263d5b1208108bfcf6de16f414a5fae2c91895
WPJ008 <wawitephilemon@gmail.com>
2026-07-04 17:26:34 +0200

    function service

---
+++ functions/services.php
@@ -0,0 +1,55 @@
+<?php
+require_once "../functions/database.php";
+
+
+function InsertService($name, $description, $price, $unit){
+
+    try{
+
+        $queryInsert = getPDO()->prepare(" INSERT INTO services(name_service, description_service, tarif_unitaire, type_tarif, created_at_service, updated_at_service) VALUES(:name, :description, :price, :unit, NOW(), NOW())");
+         
+        $queryInsert->bindParam(":name", $name);
+        $queryInsert->bindParam(":description", $description);
+        $queryInsert->bindParam(":price", $price);
+        $queryInsert->bindParam(":unit", $unit);
+      
+        $queryInsert->execute();
+
+        header("Location: ../pages/dashboard.php");
+
+    }catch(PDOException $e){
+        echo "Erreur lors de l'insertion du service : " . $e->getMessage();
+    }
+
+}
+
+
+
+
+function getAllServices(){
+
+    try{
+        $querySelect = getPDO()->prepare("SELECT * FROM services");
+        $querySelect->execute();
+        $result = $querySelect->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }catch(PDOException $e){
+        echo "Erreur lors de la récupération des services : " . $e->getMessage();
+    }
+}
+
+function getServiceById($serviceId){
+
+    try{
+        $querySelect = getPDO()->prepare("SELECT * FROM services WHERE id_service = :serviceId");
+        $querySelect->bindParam(":serviceId", $serviceId);
+        $querySelect->execute();
+        $result = $querySelect->fetch(PDO::FETCH_ASSOC);
+        return $result;
+    }catch(PDOException $e){
+        echo "Erreur lors de la récupération du service : " . $e->getMessage();
+    }
+}
+
+
+?>
