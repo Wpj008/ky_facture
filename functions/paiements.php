62fc26720a98393b7f0f1c51b02b9ef5eb8f42b0
WPJ008 <wawitephilemon@gmail.com>
2026-07-07 22:00:47 +0200

    insert log

--- functions/paiements.php
+++ functions/paiements.php
@@ -1,5 +1,6 @@
 <?php
 require_once "../functions/database.php";
+require_once "../functions/logs.php";
 
 function InsertPaiement($facture_id, $facture, $date, $montant, $mode, $reference, $commentaire, $ttc_facture){
     $pdo = getPDO();
@@ -16,6 +17,12 @@ function InsertPaiement($facture_id, $facture, $date, $montant, $mode, $referenc
         $queryInsert->bindParam(":commentaire", $commentaire);
 
         $queryInsert->execute();
+
+        InsertHistorique(
+            $_SESSION['user_id'],
+            "CREATE",
+            $_SESSION['first_name']." ".$_SESSION['last_name'] ." a enregistré un paiement de ".number_format($montant,2,","," ")." € sur la facture n° $facture." 
+        );
 
     
         if($ttc_facture > $montant){
@@ -46,6 +53,14 @@ function getAllPaiements(){
     return $query->fetchAll(PDO::FETCH_ASSOC);
 }
 
+
+
+function getAllPaiementsDashboard(){
+    $pdo = getPDO();
+    $query = $pdo->prepare("SELECT * FROM paiements INNER JOIN factures ON paiements.facture_id = factures.id_facture INNER JOIN mode_paiement ON paiements.mode_paiement_id = mode_paiement.id_mode_paiement INNER JOIN customers ON factures.customer_id = customers.id_customer ORDER BY id_paiement DESC  LIMIT 5");
+    $query->execute();
+    return $query->fetchAll(PDO::FETCH_ASSOC);
+}
 
 
 function getPaiementById($facture_id){
