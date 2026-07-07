d66e037ce17a4f77b1b3ce42397f7a7d0f52a875
WPJ008 <wawitephilemon@gmail.com>
2026-07-07 22:17:36 +0200

    traitement update

--- traitements/traitement_devis.php
+++ traitements/traitement_devis.php
@@ -4,7 +4,8 @@ session_start();
 
 require_once "../functions/devis.php";
 
-if ($_SERVER["REQUEST_METHOD"] === "POST" ) {
+
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['save_invoice']) ) {
 
 
     $customer_id = htmlspecialchars($_POST['customer_id']);
@@ -27,11 +28,34 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" ) {
 
     InsertDevis($numero_devis,$customer_id,$date_creation,$date_echeance,$status,$total_ht,$total_tva,$total_ttc,$created_by,$service,$quantite,$prix,$tva,$montant_ht,$montant_ttc);
 
-} else {
+        } elseif ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['updating_devis'])) {
 
-    header("Location: ../pages/devis.php");
-    exit;
+                $id_devis = $_POST['id_devis'];
+                $id_ligne = $_POST['id_ligne'];
+                $customer_id = htmlspecialchars($_POST['customer_id']);
+                $date_echeance = htmlspecialchars($_POST['date_validite_devis']);
+                $status = htmlspecialchars($_POST['status_devis_id']);
+                $total_ht = htmlspecialchars($_POST['total_ht']);
+                $total_tva = htmlspecialchars($_POST['total_tva']);
+                $total_ttc = htmlspecialchars($_POST['total_ttc']);
+                $quantite = $_POST['quantite'];
+                $numero_devis = $_POST['numero_devis'];
+                $montant_ht = $_POST['montant_ht'];
+                $montant_ttc = $_POST['montant_ttc'];
 
-}
+                updateDevis($id_devis, $id_ligne, $customer_id, $date_echeance, $status, $total_ht, $total_tva, $total_ttc, $quantite, $montant_ht, $montant_ttc, $numero_devis);
+
+                }  elseif($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_devis'])) {
+
+
+                $id_devis = $_POST['id_devis'];
+                
+                deleteDevis($id_devis);
+        
+        
+            }else {
+                 header("Location: ../pages/dashboard.php");
+                exit;
+        }
 
 ?>
