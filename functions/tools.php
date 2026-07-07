726e55e30464af13bc2f478a5ddce15d694475ff
WPJ008 <wawitephilemon@gmail.com>
2026-07-07 22:01:49 +0200

    insert log

--- functions/tools.php
+++ functions/tools.php
@@ -1,7 +1,12 @@
 <?php
+require_once "../functions/logs.php";
 
 function logOut(){
-
+    InsertHistorique(
+        $_SESSION['user_id'],
+        "LOGOUT",
+        $_SESSION['first_name']." ".$_SESSION['last_name']." s'est déconnecté de l'application."
+    );
 session_destroy();
 header("Location: ../index.php");
 }
