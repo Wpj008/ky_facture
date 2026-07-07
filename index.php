da3a5fcf257dfa9bb895c7cf210948f429e989aa
WPJ008 <wawitephilemon@gmail.com>
2026-07-07 22:16:10 +0200

    add css sidebar  mobile

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Connexion — Factura</title>
+  <title>Connexion — KY-Facture</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
   <link href="assets/css/style.css" rel="stylesheet">
