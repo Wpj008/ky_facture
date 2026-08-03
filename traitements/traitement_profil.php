e58fd06e2eec11eebcc99271a5f8db51dbebdab7
WPJ008 <wawitephilemon@gmail.com>
2026-08-03 22:47:00 +0200

    modification trait profil

--- traitements/traitement_profil.php
+++ traitements/traitement_profil.php
@@ -69,5 +69,46 @@ if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['update_password'])){
 }
 
 
-?>
 
+
+
+
+
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['update_customer'])){
+
+
+    if (isset($_POST["id"],$_POST["type_customer"],$_POST["lastname_customer"],$_POST["firstname_customer"],$_POST["entreprise"],$_POST["adresse_customer"],$_POST["ville_customer"],$_POST["code_postal"], $_POST["phone_customer"],$_POST["email_customer"])){
+           
+        
+        if(!empty($_POST["type_customer"]) && !empty($_POST["lastname_customer"]) && !empty($_POST["firstname_customer"]) && !empty($_POST["adresse_customer"]) && !empty($_POST["ville_customer"]) && !empty($_POST["code_postal"]) && !empty($_POST["phone_customer"]) && !empty($_POST["email_customer"])){
+       
+       
+        $id_customer = htmlspecialchars($_POST["id"]);
+
+        $type_customer = htmlspecialchars($_POST["type_customer"]);
+        $lastname_customer = htmlspecialchars($_POST["lastname_customer"]);
+        $firstname_customer = htmlspecialchars($_POST["firstname_customer"]);
+        $entreprise = htmlspecialchars($_POST["entreprise"]);
+        $adresse_customer = htmlspecialchars($_POST["adresse_customer"]);
+        $ville_customer = htmlspecialchars($_POST["ville_customer"]);
+        $code_postal = htmlspecialchars($_POST["code_postal"]);
+        $phone_customer = htmlspecialchars($_POST["phone_customer"]);
+        $email_customer = htmlspecialchars($_POST["email_customer"]);
+
+       
+            updateCustomer($id_customer,$type_customer,$lastname_customer,$firstname_customer,$entreprise,$adresse_customer,$ville_customer,$code_postal,$phone_customer,$email_customer );
+
+            header("Location: ../pages/customer.php");
+            exit();
+       
+    } else{
+        $error = "La modification a échoué.";
+    }
+  
+}
+    else{
+        $error = "Veuillez remplir tous les champs obligatoires.";
+    }
+}
+
+?>
