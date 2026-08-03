fa1da2b38991044d09021688b7d3753b8d253420
WPJ008 <wawitephilemon@gmail.com>
2026-08-03 22:44:23 +0200

    modification function

--- functions/profil.php
+++ functions/profil.php
@@ -25,7 +25,7 @@ function selectUser($id_user){// function pour selectionner les données d'un ut
 }
 
 
-    function UpdateUserProfile($id, $firstname, $lastname, $email){
+function UpdateUserProfile($id, $firstname, $lastname, $email){
 
     $query = getPDO()->prepare(" UPDATE users SET firstname_user = :firstname, lastname_user = :lastname, email_user = :email, updated_at_user = NOW() WHERE id_user = :id ");
 
@@ -66,3 +66,35 @@ function UpdatePassword($id,$password){
 
 
 }
+
+
+function updateCustomer($id_customer, $type_customer, $lastname_customer, $firstname_customer, $entreprise, $adresse_customer, $ville_customer, $code_postal, $phone_customer, $email_customer){
+   
+   
+   try{
+    
+    $request = getPDO()->prepare("UPDATE customers SET type_customer = :type_customer, lastname_customer = :lastname_customer, firstname_customer = :firstname_customer, entreprise = :entreprise, adresse_customer = :adresse_customer, ville_customer = :ville_customer, code_postal = :code_postal, phone_customer = :phone_customer, email_customer = :email_customer WHERE id_customer = :id_customer");
+
+    $request->bindParam(":type_customer", $type_customer);
+    $request->bindParam(":lastname_customer", $lastname_customer);
+    $request->bindParam(":firstname_customer", $firstname_customer);
+    $request->bindParam(":entreprise", $entreprise);
+    $request->bindParam(":adresse_customer", $adresse_customer);
+    $request->bindParam(":ville_customer", $ville_customer);
+    $request->bindParam(":code_postal", $code_postal);
+    $request->bindParam(":phone_customer", $phone_customer);
+    $request->bindParam(":email_customer", $email_customer);
+    $request->bindParam(":id_customer", $id_customer, PDO::PARAM_INT);
+
+    return $request->execute();
+
+
+} catch (PDOException $e) {
+
+    echo "Erreur lors de la mise à jour du client : " . $e->getMessage();
+    return false;
+}
+
+}
+
+?>
