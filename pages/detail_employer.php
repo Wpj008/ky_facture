53b33ae264e132205b15f8e4944cd4d426d6ad4e
WPJ008 <wawitephilemon@gmail.com>
2026-08-03 22:49:49 +0200

    update page  detail employer

--- pages/detail_employer.php
+++ pages/detail_employer.php
@@ -25,7 +25,7 @@ $factures = getFacturesByUser($id);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Studio Lumière — Factura</title>
+  <title>KY-Facture</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
   <link href="../assets/css/style.css" rel="stylesheet">
